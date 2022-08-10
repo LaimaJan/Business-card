02590d74e210e43eee7a75c27999b3a1b194da45
LaimaJan <lm.lingyte@gmail.com>
2022-08-10 20:51:38 +0300

    second commit

--- index.js
+++ index.js
@@ -1,49 +1,112 @@
-const fullName = document.querySelector("#fname").value;
-const email = document.querySelector("#email").value;
-const phone = document.querySelector("#phone").value;
-const address = document.querySelector("#address").value;
-const job = document.querySelector("#job").value;
 let btn = document.querySelector("#submit-btn");
 let newForm = document.querySelector(".published-form");
-
+let formDiv = document.querySelector(".our-form-div");
 let form = document.getElementById("bussines-card");
+const inputFields = document.querySelectorAll("input");
 
-// let ourFormKey = object key
+const isFormValid = () => {
+	var inputs = inputFields;
 
-// btn.addEventListener("click", function () {
-// 	// localStorage.setItem("fullName", fullName);
-// 	// localStorage.setItem("email", email);
-// 	// localStorage.setItem("phone", phone);
-// 	// localStorage.setItem("address", address);
-// 	// localStorage.setItem("job", job);
+	for (const input of inputs) {
+		if (input.value == "" || input.value == null) {
+			return false;
+		}
+	}
+	return true;
+};
 
-// 	const newDiv = document.createElement("div");
-// 	newDiv.classList.add("completed-form-div");
+// function validateForm() {
+// 	let a = document.forms["Form"]["fullName"].value;
+// 	let b = document.forms["Form"]["email"].value;
+// 	let c = document.forms["Form"]["phone"].value;
+// 	let d = document.forms["Form"]["address"].value;
+// 	let e = document.forms["Form"]["job"].value;
+// 	if (
+// 		(a == null || a == "",
+// 		b == null || b == "",
+// 		c == null || c == "",
+// 		d == null || d == "",
+// 		e == null || e == "")
+// 	) {
+// 		return false;
+// 	} else {
+// 		return true;
+// 	}
+// }
 
-// 	newDiv.appendChild(fullName);
-// 	newDiv.appendChild(email);
-// 	newDiv.appendChild(phone);
-// 	newDiv.appendChild(address);
-// 	newDiv.appendChild(job);
-
-// 	document.body.appendChild(newDiv);
-// 	console.log(newDiv);
-// });
 form.addEventListener("submit", submitData);
 
 function submitData(event) {
 	event.preventDefault();
+
+	if (!isFormValid()) {
+		alert("Please fill all the fields!");
+		return;
+	}
+
 	const myFormData = new FormData(event.target);
-	console.log(myFormData);
+	// console.log(myFormData);
 
 	const formDataObj = {};
 	myFormData.forEach((value, key) => (formDataObj[key] = value));
 	console.log(formDataObj);
 
 	window.localStorage.setItem("formDataObj", JSON.stringify(formDataObj));
-	let finishedCard = JSON.stringify(formDataObj);
-	console.log(finishedCard);
 
-	newForm.innerText = finishedCard;
-	document.body.appendChild(newForm);
+	// creating our card with input fields info
+	const ourPublishedForm = generateObjectElement("div", "our-published-form");
+
+	// creating cards div for img - left and text - right
+	const imgTextDiv = generateObjectElement("div", "imgTextDiv");
+	ourPublishedForm.appendChild(imgTextDiv);
+
+	formDiv.appendChild(ourPublishedForm);
+
+	// creating a div for img and info below it
+	const imgFullNameJobDiv = generateObjectElement("div", "imgFullNameJobDiv");
+	imgTextDiv.appendChild(imgFullNameJobDiv);
+
+	const img = document.createElement("img");
+	img.classList.add("profile-img");
+	img.src = "images/Ellipse2.png";
+	imgFullNameJobDiv.appendChild(img);
+
+	// creating div for full name and job title
+	const nameJob = generateObjectElement("div", "nameJob");
+	imgFullNameJobDiv.appendChild(nameJob);
+
+	nameJob.appendChild(
+		generateObjectElement("p", "fullName", formDataObj.fullName)
+	);
+
+	// creating p for job title
+	nameJob.appendChild(generateObjectElement("p", "jobTitle", formDataObj.job));
+
+	// creating div for email/phone/address
+	const infoDiv = document.createElement("div");
+	infoDiv.classList.add("infoDiv");
+	imgTextDiv.appendChild(infoDiv);
+
+	infoDiv.appendChild(generateObjectElement("p", "email", formDataObj.email));
+
+	// creating <p> for phone number
+	infoDiv.appendChild(
+		generateObjectElement("p", "phoneNumber", formDataObj.phone)
+	);
+
+	// creating <p> for address
+	infoDiv.appendChild(
+		generateObjectElement("p", "address", formDataObj.address)
+	);
+
+	function generateObjectElement(selector, className, text) {
+		const element = document.createElement(selector);
+		element.classList.add(className);
+
+		if (typeof text !== "undefined") {
+			element.innerText = text;
+		}
+
+		return element;
+	}
 }
